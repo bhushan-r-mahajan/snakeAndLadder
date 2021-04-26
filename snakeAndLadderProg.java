ad48f15d9667258171864543801b14e0fb93068b
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-26 10:39:43 +0530

    Added dice roll for the player and incremented his position accordingly

--- snakeAndLadderProg.java
+++ snakeAndLadderProg.java
@@ -4,6 +4,10 @@ public class snakeAndLadderProg {
 		System.out.println("Welcome to the Snake & Ladder Game !!");
 
 		int position = 0;
-		System.out.println("The player Position is:-" + position);
+		System.out.println("The player Position is: " + position);
+		int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+		System.out.println("Dice Roll: " + diceRoll);
+		position = position + diceRoll;
+		System.out.println("New Position of Player is: " + position);
 	}
 }
