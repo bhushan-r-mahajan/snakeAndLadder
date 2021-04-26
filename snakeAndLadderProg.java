5608530ba46bc3ca5c1b6522e203319bf68ff964
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-26 10:58:15 +0530

    Added 3 options of player check noplay, ladder, snake

--- snakeAndLadderProg.java
+++ snakeAndLadderProg.java
@@ -5,9 +5,31 @@ public class snakeAndLadderProg {
 
 		int position = 0;
 		System.out.println("The player Position is: " + position);
-		int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
-		System.out.println("Dice Roll: " + diceRoll);
-		position = position + diceRoll;
-		System.out.println("New Position of Player is: " + position);
+		while (position <= 100)
+		{
+			int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+			System.out.println("Dice Roll: " + diceRoll);
+			position = position + diceRoll;
+			System.out.println("New Position of Player is: " + position);
+			int option = (int) (Math.floor(Math.random() * 10 ) % 3 + 1);
+			switch (option)
+			{
+				case 1:
+					System.out.println("No Play");
+					System.out.println("Position: " + position);
+					break;
+				case 2:
+					System.out.println("Ladder");
+					position = position + diceRoll;
+					System.out.println("Position: " + position);
+					break;
+				case 3:
+					System.out.println("Snake");
+					position = position - diceRoll;
+					System.out.println("Position: " + position);
+					break;
+
+			}
+		}
 	}
 }
