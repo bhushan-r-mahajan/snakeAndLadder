0fef1b2aef128d6d8cfad5593959898775ff7989
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-26 10:26:37 +0530

    Added position for starting game

--- snakeAndLadderProg.java
+++ snakeAndLadderProg.java
@@ -1,6 +1,9 @@
 public class snakeAndLadderProg {
 
 	public static void main (String[] args) {
-		System.out.println("Welcome to the Snake & Ladder Game !!")
+		System.out.println("Welcome to the Snake & Ladder Game !!");
+
+		int position = 0;
+		System.out.println("The player Position is:-" + position);
 	}
 }
